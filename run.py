e7ee8c7d4b1483b38dac269c44fa9ffdc6881fe6
Holim Lim <holim0711@outlook.com>
2021-02-18 12:25:49 +0900

    functional complete!

--- run.py
+++ run.py
@@ -28,20 +28,42 @@ def read_config():
     return df, atts, vals, config['url_root']
 
 
-df, atts, vals, url_root = read_config()
+def try_parse(x):
+    if isinstance(x, str):
+        if x.isnumeric():
+            return int(x)
+    return x
+
+
+def select_df(df, args):
+    for att, val in args.items():
+        if val is not None:
+            df = df[df[att] == try_parse(val)]
+    return df
 
 
 @app.route('/')
 def viewer():
-    seed = request.args.get('seed', random.randint(0, 9999))
+    seed = request.args.get('seed')
+    seed = int(seed) if seed else random.randint(0, 9999)
     random.seed(seed)
 
     args = {x: request.args.get(x) for x in atts}
 
-    indices = random.sample(range(0, len(df)), 10)
+    rows = select_df(df, args)
 
-    images = df.iloc[indices]['url']
+    args = {
+        x: [(None, y is None)] + [(k, k == try_parse(y)) for k in vals[x]]
+        for x, y in args.items()
+    }
+
+    indices = random.sample(range(0, len(rows)), min(50, len(rows)))
+
+    images = rows.iloc[indices]['url']
 
     images = [os.path.join(url_root, x) for x in images]
 
     return render_template('viewer.html', seed=seed, images=images, args=args)
+
+
+df, atts, vals, url_root = read_config()
